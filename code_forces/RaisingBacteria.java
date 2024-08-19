e23feb159da84f3e73eea9a1c8b3f864b89d1859
lucky1218 <rahultiwari161189@gmail.com>
2024-08-19 13:54:42 +0530

    solved a problem on codeforces

---
+++ code_forces/RaisingBacteria.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class RaisingBacteria {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(Integer.bitCount(x));
+    }
+}
