41352e16130733e594f8d9f0c412a4bbfcceb513
lucky1218 <rahultiwari161189@gmail.com>
2024-09-25 11:48:44 +0530

    tried to solve 1st problem of ipc

--- code_forces/NeelsPatternChallenge.java
+++ code_forces/NeelsPatternChallenge.java
@@ -3,20 +3,38 @@ import java.util.Scanner;
 public class NeelsPatternChallenge {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt();
-        while(tc-- > 0){
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
             int n = sc.nextInt();
             String s = sc.next();
-            int ans = 0;
-            for(int i=0;i<n-2;i++){
-                int j = i;
-                if(s.charAt(i)=='0') continue;
-                while(i < n-2 && s.charAt(i)=='1' && s.charAt(i+1) == '0' && s.charAt(i+2)=='1') {ans++;i+=2;}
-                
+
+            int count = 0;
+            int lastOne = -1;
+
+            for (int i = 0; i < n; i++) {
+                if (s.charAt(i) == '1') {
+                    if (lastOne != -1) {
+                        boolean isValid = true;
+                        for (int j = lastOne + 1; j < i; j++) {
+                            if (s.charAt(j) == s.charAt(j - 1)) {
+                                isValid = false;
+                                break;
+                            }
+                        }
+                        if (isValid) {
+                            count += (i - lastOne - 1); // Number of valid substrings
+                        }
+                    }
+                    lastOne = i;
+                }
             }
-            System.out.println(ans);
+
+            System.out.println(count);
         }
+
         sc.close();
     }
 }
-// Unsolved 
+// unsolved
+
